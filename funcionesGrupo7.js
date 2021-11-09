ed095d4d8c34b9b52f8964bb5f73beb87ef1c8c7
milagrosrdzz <milagrosrdzz@gmail.com>
2021-11-09 16:40:03 -0300

    modifico formulario y validacion campos vacios

--- funcionesGrupo7.js
+++ funcionesGrupo7.js
@@ -1,35 +1,77 @@
 function validarFormulario() {
-    'use strict'
-    
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          } else{
-            confirmEnvio()
-            event.preventDefault()
-            return;
-          }
-          form.classList.add('was-validated')
-        }, false)
-      })
-  }
-  
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        } else {
+          confirmEnvio()
+          event.preventDefault()
+          return;
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+}
+
 function confirmEnvio() {
   Swal.fire({
     title: 'Se ha enviado su mensaje con éxito!!',
     icon: 'success',
     confirmButtonColor: '#CF372D'
-});  
-myForm.reset()
+  });
+  myForm.reset()
+}
+function validaRegistro() {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        } else {
+          confirmEnvio()
+          event.preventDefault()
+          return;
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
 }
 
+
+
+function SoloLetras(e) {
+  key = e.keyCode || e.which;
+  tecla = String.fromCharCode(key).toString();
+  letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚabcdefghijklmnopqáéíóú";
+
+  especiales = [0, 13];
+  tecla_especial = false
+  for (var i in especiales) {
+    if (key == especiales[i]) {
+      tecla_especial = true;
+      break;
+    }
+  }
+  if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+    alert("ingrese solo letras");
+    return false;
+  }
+}
 
 
 
