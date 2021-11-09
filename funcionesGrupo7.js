6f04984b0cc3b23f6d952dd1c6969986077565a0
milagrosrdzz <milagrosrdzz@gmail.com>
2021-11-09 18:20:58 -0300

    validacion longitud

--- funcionesGrupo7.js
+++ funcionesGrupo7.js
@@ -52,24 +52,11 @@ function validaRegistro() {
     })
 }
 
-
-
-function SoloLetras(e) {
-  key = e.keyCode || e.which;
-  tecla = String.fromCharCode(key).toString();
-  letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚabcdefghijklmnopqáéíóú";
-
-  especiales = [0, 13];
-  tecla_especial = false
-  for (var i in especiales) {
-    if (key == especiales[i]) {
-      tecla_especial = true;
-      break;
-    }
-  }
-  if (letras.indexOf(tecla) == -1 && !tecla_especial) {
-    alert("ingrese solo letras");
-    return false;
+function validaLongitud(e) {
+  if (e.value.length < 2) {
+    alert("Escriba mas de 2 caracteres");
+    e.focus();
+    e.select();
   }
 }
 
