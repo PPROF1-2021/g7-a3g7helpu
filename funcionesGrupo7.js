b1d8db1107af57595a65bc65ab91573730faf76c
Gisella Nadaya <gisemnadaya@gmail.com>
2021-11-09 21:20:12 -0300

    subo atualizaciones

--- funcionesGrupo7.js
+++ funcionesGrupo7.js
@@ -1,62 +1,45 @@
 function validarFormulario() {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        } else {
-          confirmEnvio()
-          event.preventDefault()
-          return;
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-}
-
+    'use strict'
+    
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          } else{
+            confirmEnvio()
+            event.preventDefault()
+            redirect()
+            return;
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+      
+  }
+  
 function confirmEnvio() {
   Swal.fire({
     title: 'Se ha enviado su mensaje con éxito!!',
     icon: 'success',
-    confirmButtonColor: '#CF372D'
+    footer: 'Aguarde un momento que será redireccionado al Inicio',
+    timer: '4500',
+    timerProgressBar: true,
+    showConfirmButton: false,    
+    html: '<a href="index.html">Presione aquí para ir al Inicio</a>'
   });
+  
   myForm.reset()
-}
-function validaRegistro() {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        } 
-        form.classList.add('was-validated')
-      }, false)
-    })
+  
 }
 
-function validaLongitud(e) {
-  if (e.value.length < 2) {
-    alert("Escriba mas de 2 caracteres");
-    e.focus();
-    e.select();
-  }
-}
-
-
-
+setTimeout(function redirect(){
+    window.location.href='index.html', false;
+}, 8000);
 
 
