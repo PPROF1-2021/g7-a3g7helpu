6b9c0bc66afd48ea64617dc5f135697cd929e5e4
Martin Olmedo <65569176+martinolmedo2020@users.noreply.github.com>
2021-11-10 18:00:48 -0300

    Correccion alineamiento titulos de formulario de registro

--- funcionesGrupo7.js
+++ funcionesGrupo7.js
@@ -23,6 +23,32 @@ function validarFormulario() {
       
   }
   
+function validaRegistro() {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        } 
+        form.classList.add('was-validated')
+      }, false)
+    })
+}
+
+function validaLongitud(e) {
+  if (e.value.length < 2) {
+    alert("Escriba mas de 2 caracteres");
+    e.focus();
+    e.select();
+  }
+}
 function confirmEnvio() {
   Swal.fire({
     title: 'Se ha enviado su mensaje con éxito!!',
