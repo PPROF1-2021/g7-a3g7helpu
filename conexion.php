35a66865142550ff1c133462eb1fa6240cb04865
Martin Olmedo <65569176+martinolmedo2020@users.noreply.github.com>
2021-11-17 10:03:46 -0300

    Modificacion en los archivos del backend

--- conexion.php
+++ conexion.php
@@ -4,9 +4,9 @@
     $contraseña="+1)US|sA?7)%kk\Z";
     $nombd="id17547978_helpu";
 
-    $conexion=new mysqli($host,$usuario,$contraseña,$nombd);
-
-    if($conexion){
+    $conn=new mysqli($host,$usuario,$contraseña,$nombd);
+    mysqli_query($conn , SET character_set_result-utf8);
+    if($conn){
         echo "Conexion exitosa";
     } else {
         echo "No se realizo la conexion a la base de datos";
--- guardar_registro.php
+++ guardar_registro.php
@@ -1,6 +1,5 @@
 <?php
-    include("conexion.php");
-
+  
     $nombre=$_POST['username'];
     $apellido=$_POST['surname'];
     $nacimiento=$_POST['trip-start'];
@@ -10,17 +9,49 @@
     $provincia=$_POST['province'];
     $pais=$_POST['country'];
     $email=$_POST['email'];
-    $contraseña=$_POST['pwd'];
+    $contraseñaR=$_POST['pwd'];
+
+    print "     <p>Su nombre es <strong>$nombre</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su apellido es <strong>$apellido</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su fecha de nacimiento es <strong>$nacimiento</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su direccion es <strong>$direccion</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su ciudad es <strong>$ciudad</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su codigo postal es <strong>$cpostal</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su provincia es <strong>$provincia</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su pais es <strong>$pais</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su email es <strong>$email</strong>.</p>\n";
+    print "\n";
+    print "     <p>Su contraseña es <strong>$contraseñaR</strong>.</p>\n";
+    print "\n";
+
+    //Incluimos los datos de conexion y las funciones
+
+    include("conexion.php");
+    $con = mysqli_connect($host,$usuario,$contraseña,$nombd) or die ("No se ha podido conectar al servidor de la base de datos");
+    if(!con){
+        die("Conexion fallida: ") . mysqli_connect_error());
+    }
+
+    $db = mysqli_select_db($con, $nombd) or die ("Ups no se ha podido conectar a la base de datos");
 
     //INSERTAR DATOS
-    $query="INSERT INTO Cliente(Nombre,Apellido,Nacimiento,Direccion,CP,Provincia,Pais,Email,Contraseña) VALUES('$nombre','$apellido','$nacimiento','$direccion','$ciudad','$codigopostal','$provincia','$pais','$email','$contraseña')";
+    $consulta="INSERT INTO Cliente (Nombre, Apellido, Nacimiento, Direccion, CP, Provincia, Pais, Email,Contraseña) VALUES('$nombre','$apellido','$nacimiento','$direccion','$ciudad','$codigopostal','$provincia','$pais','$email','$contraseñaR')";
 
-    //VERIFICACION
-    $resultado=$conexion->query($query);
-
-    if($resultado){
-        echo "Datos insertados correctamente";
+    if(mysqli_query($con, $consulta)) {
+        echo "<p>Registro agregado</p>";
     } else {
-        echo "No se insertaron los datos";
+        echo "<p>No se agrego el registro</p>";
+        echo "Error: " . $consulta . "<br>" . mysqli_error($con);
     }
+    mysqli_close($con);
+
+
 ?>
