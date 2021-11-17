2a5f335cbba2d3208651c6060c7840fef207f5bf
Martin Olmedo <65569176+martinolmedo2020@users.noreply.github.com>
2021-11-17 08:09:16 -0300

    Codigo backend de insercion de datos del registro en la BD

---
+++ guardar_registro.php
@@ -0,0 +1,17 @@
+<?php
+    include("conexion.php");
+
+    $nombre=$_POST['username'];
+    $apellido=$_POST['surname'];
+    $nacimiento=$_POST['trip-start'];
+    $direccion=$_POST['adress'];
+    $ciudad=$_POST['city'];
+    $codigopostal=$_POST['cpostal'];
+    $provincia=$_POST['province'];
+    $pais=$_POST['country'];
+    $email=$_POST['email'];
+    $contraseña=$_POST['pwd'];
+
+    //INSERTAR DATOS
+    $query="INSERT INTO Cliente(Nombre,Apellido,Nacimiento,Direccion,CP,Provincia,Pais,Email,Contraseña) VALUES('$nombre','$apellido','$nacimiento','$direccion','$ciudad','$codigopostal','$provincia','$pais','$email','$contraseña')"
+?>
