4b5a61175d5949d81dcdefa5cadf94525fe95e7b
Martin Olmedo <65569176+martinolmedo2020@users.noreply.github.com>
2021-11-18 20:46:40 -0300

    Se mueve la funcion de fecha al archivo js y se aplica en todas las paginas

--- funcionesGrupo7.js
+++ funcionesGrupo7.js
@@ -145,3 +145,18 @@ function mostrarDatosDeRegistro(){
     window.location.href='index.html', false;
   }, 4500);
 }
+
+function copyrightInicio() {
+  
+  var parrafoCopyright = document.getElementById('copyright-helpU');
+  var meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+  var diasSemana = new Array(" Domingo", " Lunes", " Martes", " Miércoles", " Jueves", " Viernes", " Sábado");
+  var f = new Date();
+  var mensajeCopyright = "© helpU - Todos los derechos reservados " + diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear();
+  var mensajeATexto = mensajeCopyright.toString();
+
+  parrafoCopyright.innerText = mensajeATexto;
+}
+
+copyrightInicio();
+
